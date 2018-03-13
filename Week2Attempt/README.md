1617ddd0b4df4e3e32a38ea143c86295fb156c36
Chido Nguyen <chidonguyen@hotmail.com>
2018-03-12 19:06:18 -0700

    2 user story of week 2
    Works, but need verification

--- Week2Attempt/README.md
+++ Week2Attempt/README.md
@@ -1,22 +1,13 @@
-StoriesWeekOne.py contains a demonstration of this week's user stories written in Pyton 3 which depends upon code in Schema.py, UserHelpers.py, and DonationHelpers.py. 
+W2_UserHelpers.py contains receiver specific functions, Did not want to add them to the main document for clarity sake.
 
-Run with "python3 StoriesWeekOne.py"
+StoriesWeekone.py:
+	Imports the W2_UserHelpers functions in
+	Added Test_8 and Test_9 to test the 2 user stories hopefully its accurately doing it.
+	
+getOpenDonations():
+	Pulls any donations in the table that hasn't been updated with a timestamp i.e. still open
 
-Schema.py contains a sqlite3 implementation of the following tables: 
-	users(pid, perms, uid, hash) 
-	donations(id, provider, receiver, created, completed) 
-	items(id, did, barcode, title, count, units)
-	barcodes(code, title, units)
+getReceiverDonations():
+	Pulls donation packages specific to a receiver_uid (rid), AND has its package marked as completed (i.e. not 0)
+	
 
-UserHelpers.py contains the following user-level functions: 
-	validUser() - Validates a user/pass pair
-	writeUser() - Writes to user table, creating or updating a user account
-
-DonationHelpers.py contains the following donation-level functions:
-	addBarcode() adds a new barcode to the barcodes table 
-	addDonation() creates a new empty donation 
-	addItemByManual() adds a new item to the items table with manual values 
-	addItemByBarcode() adds a new item to the items table pulling barcode data 
-	listProviderDonations() lists pending and/or past donations
-	getDonationItems() returns all items in a donation by donation id 
-	claimDonation() updates donation.receiver value 
--- Week2Attempt/W2_UserHelpers.py
+++ Week2Attempt/W2_UserHelpers.py
@@ -20,10 +20,10 @@ def getOpenDonations(db):
 def getReceiverDonations(db,rid):
 
         c = db.cursor()
-        c.execute('''SELECT * FROM donations WHERE receiver = ? AND complete = ?''',(rid,0,))
+        c.execute('''SELECT * FROM donations WHERE receiver = ? AND completed != ?''',(rid,0,))
         results = c.fetchall()
         c.close
-        return result
+        return results
 
 
 #W2 User Story 1: As a receiver, I can see open donations
@@ -39,25 +39,24 @@ def testReceiverOpenDonations(test,db):
             print('No opoen donations found.')
 
         #donations found
-        else
+        else:
             print('\n\t\t\t Open Donations')
             for r in result:
                 print('\nrow\tprovider\treceiver\tcreated\t\t\t\tcompleted')
-		print('{0}\t{1}\t\t{2}\t\t{3}\t{4}'.format(r[0], r[1], r[2], r[3], r[4]))
+                print('{0}\t{1}\t\t{2}\t\t{3}\t{4}'.format(r[0], r[1], r[2], r[3], r[4]))
 
 #W2 User Story 2: As a receiver, I can see my past donations
 #Syntax ( <test_dict>, db, receiver)
 def testReceiverHistory(test,db,receiver):
 
         test['receiverHistory'][0] += 1
-        retult = getReceiverDonations(db, receiver)
+        result = getReceiverDonations(db, receiver)
 
         if result is []:
             test['receiverHistory'][1] +=1
             print('No past donations found.')
-        else
-        print('\n\t\t\t\tPast Donations')
-		for r in result:
-			print('\nrow\tprovider\treceiver\tcreated\t\t\t\tcompleted')
-			print('{0}\t{1}\t\t{2}\t\t{3}\t{4}'.format(r[0], r[1], r[2], r[3], r[4]))
-
+        else:
+            print('\n\t\t\t\tPast Donations')
+            for r in result:
+                    print('\nrow\tprovider\treceiver\tcreated\t\t\t\tcompleted')
+                    print('{0}\t{1}\t\t{2}\t\t{3}\t{4}'.format(r[0], r[1], r[2], r[3], r[4]))
